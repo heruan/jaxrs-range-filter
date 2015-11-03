36304a844c16f114928da009f38782a4d89bd68d
Giovanni Lovato <heruan@aldu.net>
2015-11-03 19:01:09 +0100

    Added support for If-Range header

--- src/main/java/to/lova/jersey/filter/RangeResponseFilter.java
+++ src/main/java/to/lova/jersey/filter/RangeResponseFilter.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response.Status;
 
 /**
@@ -15,22 +16,53 @@ import javax.ws.rs.core.Response.Status;
  */
 public class RangeResponseFilter implements ContainerResponseFilter {
 
-    private static final String RANGE = "Range";
-    
     private static final String ACCEPT_RANGES = "Accept-Ranges";
-    
+
+    private static final String BYTES_RANGE = "bytes";
+
+    private static final String RANGE = "Range";
+
+    private static final String IF_RANGE = "If-Range";
+
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
+
+        responseContext.getHeaders().add(ACCEPT_RANGES, BYTES_RANGE);
+
         if (requestContext.getHeaders().containsKey(RANGE)) {
-            String rangeHeader = requestContext.getHeaderString(RANGE);
-            String contentType = responseContext.getMediaType().toString();
-            OutputStream originOutputStream = responseContext.getEntityStream();
-            RangedOutputStream rangedOutputStream = new RangedOutputStream(originOutputStream, rangeHeader, contentType, responseContext.getHeaders());
-            responseContext.setStatus(Status.PARTIAL_CONTENT.getStatusCode());
-            responseContext.getHeaders().putSingle(ACCEPT_RANGES, rangedOutputStream.getAcceptRanges());
-            responseContext.setEntityStream(rangedOutputStream);
+            if (requestContext.getHeaders().containsKey(IF_RANGE)) {
+                String ifRangeHeader = requestContext.getHeaderString(IF_RANGE);
+                if (responseContext.getHeaders().containsKey(HttpHeaders.ETAG)) {
+                    if (responseContext.getHeaderString(HttpHeaders.ETAG).equals(ifRangeHeader)) {
+                        this.applyFilter(requestContext, responseContext);
+                        return;
+                    }
+                }
+                if (responseContext.getHeaders().containsKey(HttpHeaders.LAST_MODIFIED)) {
+                    if (responseContext.getHeaderString(HttpHeaders.LAST_MODIFIED).equals(ifRangeHeader)) {
+                        this.applyFilter(requestContext, responseContext);
+                        return;
+                    }
+                }
+            } else {
+                this.applyFilter(requestContext, responseContext);
+            }
         }
+
+    }
+
+    private void applyFilter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        String rangeHeader = requestContext.getHeaderString(RANGE);
+        String contentType = responseContext.getMediaType().toString();
+        OutputStream originOutputStream = responseContext.getEntityStream();
+        RangedOutputStream rangedOutputStream = new RangedOutputStream(originOutputStream, rangeHeader, contentType,
+                responseContext.getHeaders());
+        responseContext.setStatus(Status.PARTIAL_CONTENT.getStatusCode());
+        responseContext.setEntityStream(rangedOutputStream);
+
     }
 
 }
